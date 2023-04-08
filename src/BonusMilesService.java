1bc92c511f642b3648cc5d67a63804ff57ab7cf4
Vadim <vadim.penkin1997@mail.ru>
2023-04-09 00:17:55 +0300

    Initial commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
 
-    public int calculate(int price){
-        int result = price/20;
+    public int calculate(int price) {
+        int result = price / 20;
         return result;
     }
 
